3c8051341b521607d2e1743d342b25fa470135a1
TimmyLuong11 <Timothy.luong@ou.edu>
2021-05-04 20:42:21 -0500

    Working code

---
+++ JCafe/Cafe.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JCafe
+{
+    class Cafe
+    {
+        const double taxrate = 0.055;
+        public string CustName { get; set; }
+        public string Food { get; set; }
+        public string Drink { get; set; }
+        public string Toppings { get; set; }
+        public double Amount { get; set; }
+
+        public Cafe()
+        {
+            CustName = string.Empty;
+            Food = string.Empty;
+            Drink = string.Empty;
+            Toppings = string.Empty;
+            Amount = 0;
+        }
+
+        public string FoodMenu()
+        {
+            string food = (
+                               "\n\tFood:" +
+
+                                "\n\tVietnamese Pizza------$10.00" +
+
+                                "\n\tPho-----------------------$15.00" +
+
+                                "\n\tEggrolls---------------$5.00" +
+
+                                "\n\tBanh Mi--------------$8.00");
+            return food;
+        }
+        public string DrinkMenu()
+        {
+            string drink = (
+                               "\n\tDrinks:" +
+
+                                "\n\tBrown Sugar Boba------$3.50" +
+
+                                "\n\tTaro Milk Tea----------------$4.00" +
+
+                                "\n\tHoneydew Milk Tea------------$4.00" +
+
+                                "\n\tVietnamese Coffee----------$3.00");
+
+
+
+            return drink;
+        }
+        public string ToppingsMenu()
+        {
+            string Tops =
+
+                            ("\n\tToppings:" +
+                            "\n\tTapioca----0.50" +
+                            "\n\tJelly-----0.50" +
+                            "\n\tPopping Boba---0.50");
+            return Tops;
+        }
+
+        public double CalculatePrice(string item)
+        {
+
+            double price = 0;
+            if (item == "vietnamese pizza")
+            {
+                price = 10.00;
+            }
+            else if (item == "pho")
+            {
+                price = 15.00;
+            }
+            else if (item == "eggrolls")
+            {
+                price = 5.00;
+            }
+            else if (item == "banh mi")
+            {
+                price = 8.00;
+            }
+            else if (item == "brown sugar boba")
+            {
+                price = 4.00;
+            }
+            else if (item == "taro milk tea")
+            {
+                price = 4.00;
+            }
+            else if (item == "honeydew milk tea")
+            {
+                price = 4.00;
+            }
+            else if (item == "vietnamese coffee")
+            {
+                price = 3.00;
+            }
+            else if (item == "tapioca")
+            {
+                price = 0.50;
+            }
+            else if (item == "jelly")
+            {
+                price = 0.50;
+            }
+            else if (item == "popping boba")
+            {
+                price = 0.50;
+            }
+            return price;
+        }
+        public void Receipt(Cafe subtotal)
+        {
+            double tax = subtotal.Amount * taxrate;
+            double total = tax + subtotal.Amount;
+            Console.WriteLine($"Subtotal of your item(s) is {subtotal.Amount.ToString("C")}");
+            Console.WriteLine($"Tax of your item(s) is {tax.ToString("C")}");
+            Console.WriteLine($"Total  of your item(s) is {total.ToString("C")}");
+        }
+        public string DisplayOrder(string item)
+        {
+            //Console.WriteLine($"You ordered:\n{item}");
+            return ($"You ordered:\n{item}");
+            //return $"{item.Food} \n{item.Drink} \n{item.Toppings}";
+        }
+
+        public override string ToString()
+        {
+            return $"{Food} \n{Drink} \n{Toppings}";
+        }
+    }
+}
---
+++ JCafe/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JCafe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<Cafe>> CustNam= new Dictionary<string, List<Cafe>>();
+            //List<Cafe> custOrder = new List<Cafe>();
+            string name, answer, response, order = string.Empty, food = string.Empty, drink = string.Empty, top = string.Empty;
+            double total = 0, price = 0;
+
+
+            Cafe C = new Cafe();
+
+            Console.WriteLine("**. **. **Welcome to Café Penguin**.**.** ");
+
+            //figure out how to add name into list
+            do
+            {
+
+                Console.WriteLine("Please type in your name >.<");
+                name = Console.ReadLine();
+                C.CustName = name;
+                CustNam.Add(name, new List<Cafe>());
+
+            do
+            {
+                total = 0;
+                Console.WriteLine(C.FoodMenu());
+                Console.WriteLine("What would you like to order: ");
+                answer = Console.ReadLine().ToLower();
+                food = answer;
+                price = C.CalculatePrice(answer);
+                total += price;
+                Console.WriteLine("Would you like a drink: yes or no");
+                response = Console.ReadLine().ToLower();
+                if (response == "yes")
+                {
+                    Console.WriteLine(C.DrinkMenu());
+                    Console.WriteLine("What kind of drink would you like: ");
+                    answer = Console.ReadLine().ToLower();
+                    drink = answer;
+                    price = C.CalculatePrice(answer);
+                    total += price;
+
+                }
+                else
+                {
+                    Console.WriteLine("Cool beans! ");
+                }
+                Console.WriteLine("Would you like toppings with your drink: yes or no:");
+                response = Console.ReadLine().ToLower();
+                if (response == "yes")
+                {
+                    Console.WriteLine(C.ToppingsMenu());
+                    Console.WriteLine("What kind of toppings would you like: ");
+                    answer = Console.ReadLine().ToLower();
+                    top = answer;
+                    //custOrder.Add(C);
+                    price = C.CalculatePrice(answer);
+                    total += price;
+                }
+                else
+                {
+                    Console.WriteLine("Cool beans! ");
+                }
+                C = new Cafe()
+                {
+                    Food = food,
+                    Drink = drink,
+                    Toppings = top,
+                    Amount = total,
+            };
+                    CustNam[name].Add(C); 
+                //custOrder.Add(C);
+                C.Amount = total;
+
+                Console.WriteLine($"Would you like to order anything else: ");
+                order = Console.ReadLine();
+            } while (order.ToLower() == "yes");
+                Console.WriteLine("would you like to another perosn");
+                order = Console.ReadLine();
+            } while (order.ToLower() == "yes");
+            //portion that would allow the food and drink menu
+
+
+            //output the receipt total and the user's name
+
+            foreach (var item in CustNam)
+            {
+            Console.WriteLine($"{item.Key}'s order is:");
+            for (int i = 0; i < item.Value.Count; i++)
+            {
+                Console.WriteLine(item.Value[i]);
+                C.Receipt(item.Value[i]);
+            }
+             }
+        }
+    }
+}
